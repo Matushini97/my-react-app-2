43eb08236d2fecc0d3556afb2d8e1d18b7afee27
Ilya Matyushin <IVMatyushin@alfabank.ru>
2025-08-06 14:34:56 +0300

    add cookie display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cookies, setCookies] = useState<string>("");
+
   useEffect(() => {
     document.cookie = "crossCookie=test; path=/; SameSite=None; Secure";
+
+    const allCookies = document.cookie;
+    setCookies(allCookies);
   }, []);
-  return <div>App 2</div>;
+
+  return (
+    <div>
+      <h1>App 2</h1>
+      <p>Все существующие куки:</p>
+      <pre>{cookies || "Кук нет"}</pre>
+    </div>
+  );
 }
 
 export default App;
